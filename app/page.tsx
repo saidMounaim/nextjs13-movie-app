18ff571707e789d2683ba701e817d06985afac7b
Said Mounaim <saidmounaim00@gmail.com>
2023-01-24 16:05:21 +0100

    Show Popular Movies On Home Page

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,4 @@
-import Link from "next/link";
+import PopularMovies from "@/components/PopularMovies";
 
 async function getMovies(type: string) {
   const res = await fetch(
@@ -15,15 +15,7 @@ export default async function Home() {
   return (
     <main className="mt-5 flex flex-col">
       <div className="w-[1300px] max-w-full px-4 mx-auto">
-        <div className="flex justify-between items-center">
-          <h1 className="text-2xl font-medium">Popular Movies</h1>
-          <Link
-            href="/movies/popular"
-            className="py-2 px-5 bg-slate-800 text-md font-normal text-white"
-          >
-            See all
-          </Link>
-        </div>
+        <PopularMovies popularMovies={popularMovies} />
       </div>
     </main>
   );
