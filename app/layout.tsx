e3a3555c2ec26333d60d46ebf3f932e90965f74a
Said Mounaim <saidmounaim00@gmail.com>
2023-01-24 12:48:26 +0100

    Fetch Popular Movies

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,9 +1,10 @@
-import './globals.css'
+import Header from "@/components/Header";
+import "./globals.css";
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
     <html lang="en">
@@ -12,7 +13,10 @@ export default function RootLayout({
         head.tsx. Find out more at https://beta.nextjs.org/docs/api-reference/file-conventions/head
       */}
       <head />
-      <body>{children}</body>
+      <body>
+        <Header />
+        {children}
+      </body>
     </html>
-  )
+  );
 }
--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,28 @@
-export default function Home() {
+import Link from "next/link";
+
+async function getPopularMovies() {
+  const res = await fetch(
+    `https://api.themoviedb.org/3/movie/popular?api_key=${process.env.API_KEY}&language=en-US`
+  );
+  return res.json();
+}
+
+export default async function Home() {
+  const popularMovies = await getPopularMovies();
+
   return (
-    <main>
-      <h1 className="text-xl font-medium">Movie App</h1>
+    <main className="mt-5 flex flex-col">
+      <div className="w-[1300px] max-w-full px-4 mx-auto">
+        <div className="flex justify-between items-center">
+          <h1 className="text-2xl font-medium">Popular Movies</h1>
+          <Link
+            href="/movies/popular"
+            className="py-2 px-5 bg-slate-800 text-md font-normal text-white"
+          >
+            See all
+          </Link>
+        </div>
+      </div>
     </main>
   );
 }
