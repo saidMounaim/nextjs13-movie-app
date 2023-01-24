7e828b989b31e5a2a8ea615a8aff366bd850e158
Said Mounaim <saidmounaim00@gmail.com>
2023-01-24 12:52:41 +0100

    Fecth Top Rated & Upcoming Movies

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,16 @@
 import Link from "next/link";
 
-async function getPopularMovies() {
+async function getMovies(type: string) {
   const res = await fetch(
-    `https://api.themoviedb.org/3/movie/popular?api_key=${process.env.API_KEY}&language=en-US`
+    `https://api.themoviedb.org/3/movie/${type}?api_key=${process.env.API_KEY}&language=en-US`
   );
   return res.json();
 }
 
 export default async function Home() {
-  const popularMovies = await getPopularMovies();
+  const popularMovies = await getMovies("popular");
+  const topRatedMovies = await getMovies("top_rated");
+  const upcomingMovies = await getMovies("upcoming");
 
   return (
     <main className="mt-5 flex flex-col">
